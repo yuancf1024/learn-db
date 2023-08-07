2976007507192e684251e5c3348ab7bc9f2ed80f
yuancf1024 <3455911441@qq.com>
2023-08-08 00:37:57 +0800

    修改主函数，支持两个新关键字

--- build-a-simple-db-in-c/db.c
+++ build-a-simple-db-in-c/db.c
@@ -55,10 +55,31 @@ int main(int argc, char* argv[]) {
         read_input(input_buffer);
 
         if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
         } else {
             printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGINED_COMMAND):
+                    printf("Unrecognied command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
+        }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNCOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n",
+                    input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
