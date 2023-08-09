b092cde559c2c7956d10590d8b37e65cd7a1a4a5
yuancf1024 <3455911441@qq.com>
2023-08-10 00:03:46 +0800

    定义表结构 & 计算表行数

--- build-a-simple-db-in-c/db.c
+++ build-a-simple-db-in-c/db.c
@@ -63,6 +63,17 @@ const uint32_t USERNAME_OFFSET = ID_OFFSET +ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
+// 一个 Table结构指向行的页面，并跟踪有多少行
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+
+// 定义Table结构
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
 // 向用户打印提示符
 void print_prompt() {
     printf("db > ");
