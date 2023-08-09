db97430876069bf2460223bdcf6a5a2df2f7c2c0
yuancf1024 <3455911441@qq.com>
2023-08-09 23:49:56 +0800

    定义行的紧凑表示形式

--- build-a-simple-db-in-c/db.c
+++ build-a-simple-db-in-c/db.c
@@ -52,6 +52,17 @@ typedef struct {
     Row row_to_insert; // only used by insert statement
 } Statement;
 
+// 定义行的紧凑表示形式
+# define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET +ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
 // 向用户打印提示符
 void print_prompt() {
     printf("db > ");
