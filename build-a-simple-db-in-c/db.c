1c967633c92aaf4fbff48ec768be95c95ae35749
yuancf1024 <3455911441@qq.com>
2023-10-24 22:30:33 +0800

    add 第3部分 内存中仅追加的单表数据库

--- build-a-simple-db-in-c/db.c
+++ build-a-simple-db-in-c/db.c
@@ -23,6 +23,12 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+// 定义执行结果枚举
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
+
 // 定义元命令的枚举
 typedef enum {
     META_COMMAND_SUCCESS,
@@ -31,6 +37,7 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
@@ -67,12 +74,62 @@ const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 // 定义Table结构
 typedef struct {
     uint32_t num_rows;
     void* pages[TABLE_MAX_PAGES];
 } Table;
+
+// 打印行函数
+void print_row(Row* row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+// 转换Row到紧凑表示
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+// 从紧凑表示转换到Row
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// 如何计算特定行在内存中的读/写位置的方法
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        // 仅在尝试访问页时分配内存
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+// 初始化表，创建相应的内存释放函数，并处理一些更多的错误情况
+Table* new_table() {
+    Table* table = (Table*) malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table) {
+    for (int i = 0; table->pages[i]; i++) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
 
 // 向用户打印提示符
 void print_prompt() {
@@ -133,47 +190,43 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-// “虚拟机”
-void execute_statement(Statement* statement) {
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row* row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+// 实现execute_statement的读/写
+ExecuteResult execute_statement(Statement* statement, Table* table) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
-            printf("This is where we would do an insert.\n");
-            break;
+            return execute_insert(statement, table);
         case (STATEMENT_SELECT):
-            printf("This is where we would do a select.\n");
-            break;
+            return execute_select(statement, table);
     }
 }
 
-// 转换Row到紧凑表示
-void serialize_row(Row* source, void* destination) {
-    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
-}
 
-// 从紧凑表示转换到Row
-void deserialize_row(void* source, Row* destination) {
-    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
-    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
-}
-
-// 如何计算特定行在内存中的读/写位置的方法
-void* row_slot(Table* table, uint32_t row_num) {
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = table->pages[page_num];
-    if (page == NULL) {
-        // 仅在尝试访问页时分配内存
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-    return page + byte_offset;
-}
 
 // DB入口
 int main(int argc, char* argv[]) {
+    Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
@@ -197,14 +250,33 @@ int main(int argc, char* argv[]) {
             switch (prepare_statement(input_buffer, &statement)) {
                 case (PREPARE_SUCCESS):
                     break;
+                case (PREPARE_SYNTAX_ERROR):
+                    printf("Syntax error. Could not parse statement.\n");
+                    continue;
                 case (PREPARE_UNRECOGNIZED_STATEMENT):
                     printf("Unrecognized keyword at start of '%s'.\n",
                         input_buffer->buffer);
                     continue;
             }
 
-            execute_statement(&statement);
-            printf("Executed.\n");
+            switch (execute_statement(&statement, table)) {
+                case (EXECUTE_SUCCESS):
+                    printf("Executed.\n");
+                    break;
+                case (EXECUTE_TABLE_FULL):
+                    printf("Error: Table full.\n");
+                    break;
+            }
         }
     }
 }
+
+/**
+ * @brief 
+ * 第3部分 内存中仅追加的单表数据库
+ * > insert 1 cstack foo@bar.com
+ * > insert 2 bob bob@example.com
+ * > select
+ * > insert foo bar 1
+ * > .exit
+ */
